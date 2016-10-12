91b626793b3eba3ab66f7a8c17a8af57211ee9a6
Trym Moeller <trym@tlt.local>
2016-10-12 13:42:29 +0200

    Initial version

---
+++ src/main/scala/ae/teletronics/nlp/w2vec/W2VecManager.scala
@@ -0,0 +1,40 @@
+package ae.teletronics.nlp.w2vec
+
+import java.io.File
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.mllib.feature.{Word2Vec, Word2VecModel}
+
+/**
+  * Created by trym on 11-10-2016.
+  */
+class W2VecManager extends Lifecycle {
+  private val modelPath = "spark-w2vec-model"
+  private lazy val sc: SparkContext = initSC()
+  private lazy val model = word2VecModel(sc)
+
+  override def shutDown(): Unit = sc.stop()
+
+  def usedInSameContext(term: String): List[(String, Double)] = model.findSynonyms(term, 40).toList
+
+  private def word2VecModel(sc: SparkContext) = {
+    if (new File(modelPath).exists()) {
+      Word2VecModel.load(sc, modelPath)
+    } else {
+      val file = "C:\\Users\\trym\\Downloads\\text8" // http://mattmahoney.net/dc/text8.zip
+      //      val file = "C:\\Users\\trym\\Downloads\\text8_partaa_partaa"
+      val input = sc.textFile(file).map(line => line.split(" ").toSeq)
+      val word2vec = new Word2Vec() // https://spark.apache.org/docs/2.0.1/api/java/org/apache/spark/mllib/feature/Word2Vec.html
+      //      word2vec.setMinCount(100) // TODO this should resemble the size of the learning data (default is 5)
+      val model: Word2VecModel = word2vec.fit(input)
+      model.save(sc, modelPath)
+      model
+    }
+  }
+
+  private def initSC() = {
+    val conf = new SparkConf().setAppName("Word2Vector").setMaster("local")
+    new SparkContext(conf)
+  }
+
+}
---
+++ src/test/scala/ae/teletronics/nlp/w2vec/W2VecManagerTest.scala
@@ -0,0 +1,40 @@
+package ae.teletronics.nlp.w2vec
+
+import org.apache.log4j.{Level, Logger}
+import org.junit.{After, Before, Test}
+
+/**
+  * Created by trym on 11-10-2016.
+  */
+class W2VecManagerTest {
+
+  private var loggerLevel: org.apache.log4j.Level = _
+
+  @Before
+  def setup: Unit = {
+    loggerLevel = Logger.getRootLogger.getLevel
+    Logger.getRootLogger().setLevel(Level.OFF);
+    Logger.getLogger("org.apache.spark").setLevel(Level.OFF)
+  }
+
+  @After
+  def teardown: Unit = {
+    Logger.getRootLogger().setLevel(loggerLevel);
+  }
+
+  @Test
+  def testUsedInSameContext() = {
+    val w2VecManager = new W2VecManager()
+
+    measure("anarchist", w2VecManager)
+    measure("hero", w2VecManager)
+
+    w2VecManager.shutDown()
+  }
+  private def measure(term: String, manager: W2VecManager) = {
+    val startT = System.currentTimeMillis()
+    manager.usedInSameContext(term).foreach(println)
+    println(s"\nusedInSameContext(${term}) took ${System.currentTimeMillis() - startT}")
+  }
+
+}
